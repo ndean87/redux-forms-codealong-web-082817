1262c3b5ecb02792bce5c1be5fe4cedde5846e2e
ndean87 <ndean87@gmail.com>
2017-11-17 09:53:44 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,12 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {todos: state.todos.concat(action.todo.text)}
+    default:
+      return state;
+
+  }
   return state;
 }
